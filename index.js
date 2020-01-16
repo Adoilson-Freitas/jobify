01cfb5be291d661d5c151a94fb96e172859b68ef
Adoilson Freitas <adoilsonfreitas@gmail.com>
2020-01-15 22:49:33 -0300

    blocking admin from non localhost

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 const express = require("express");
 const app = express();
-const promise = require("promise");
 const bodyParser = require("body-parser");
 
 const path = require("path");
 
 const sqlite = require("sqlite");
 const dbConnection = sqlite.open(path.resolve(__dirname, "banco.sqlite"), {
-  promise
+  Promise
 });
 
 const port = process.env.PORT || 3000;
